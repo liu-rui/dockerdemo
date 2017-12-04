6487ad5a4bb231cc34ab9e730e0445b8acc3e0a9
liurui <1313475@qq.com>
2017-12-04 11:25:46 +0800

    update

---
+++ 2.docker-compose/2web+redis/demo/src/main/java/com/github/liurui/demo/demo/DemoApplication.java
@@ -0,0 +1,12 @@
+package com.github.liurui.demo.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
--- 2.docker-compose/2web+redis/demo/src/main/java/com/github/liurui/demo/demo/controllers/UserController.java
+++ 2.docker-compose/2web+redis/demo/src/main/java/com/github/liurui/demo/demo/controllers/UserController.java
@@ -1,4 +1,23 @@
 package com.github.liurui.demo.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    RedisTemplate redisTemplate;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String get() {
+        Long a = redisTemplate.opsForValue().increment("a", 1);
+        return "hello " + a;
+    }
 }
